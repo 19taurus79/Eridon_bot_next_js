5ae64794263ba08d7cff554ef8b83e979232bf66
19taurus79 <19taurus79@gmail.com>
2025-06-21 16:08:19 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
+// import Footer from "@/components/Footer/Footer";
 import Script from "next/script";
 
 const geistSans = Geist({
